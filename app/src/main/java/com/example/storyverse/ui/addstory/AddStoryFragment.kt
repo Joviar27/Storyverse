ff1e62e4e685b34009b26ca5f46cfb3071f3cbf5
Joviar27 <joviar27@gmail.com>
2023-05-13 23:46:09 +0700

    remove location when disabled

--- app/src/main/java/com/example/storyverse/ui/addstory/AddStoryFragment.kt
+++ app/src/main/java/com/example/storyverse/ui/addstory/AddStoryFragment.kt
@@ -202,6 +202,9 @@ class AddStoryFragment : Fragment(), MenuProvider {
                 getMyLastLocation()
                 buttonView.isChecked = switchState
             }
+            else{
+                latLng = null
+            }
         }
     }
 
